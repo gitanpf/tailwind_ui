be00ab64d0aa365c7c2c2059f57645cf0a99930c
contaparaestudo-uwu <contato.anpf@gmail.com>
2026-04-27 17:47:33 -0300

    Adicionando o next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/nome-do-seu-repositorio',
+  images: {
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
